cfe39865bccfc226b1e6f48a984623df0b64a10c
softdream815 <uyg0815@gmail.com>
2016-09-17 18:56:00 -0700

    adds missing tr tags for table headers

--- tests/BridgeAccessTokenRepositoryTest.php
+++ tests/BridgeAccessTokenRepositoryTest.php
@@ -13,9 +13,9 @@ class BridgeAccessTokenRepositoryTest extends PHPUnit_Framework_TestCase
     {
         $expiration = Carbon::now();
 
-        $tokenRepository = Mockery::mock('Laravel\Passport\TokenRepository');
+        $database = Mockery::mock('Illuminate\Database\Connection');
 
-        $tokenRepository->shouldReceive('create')->once()->andReturnUsing(function ($array) use ($expiration) {
+        $database->shouldReceive('table->insert')->once()->andReturnUsing(function ($array) use ($expiration) {
             $this->assertEquals(1, $array['id']);
             $this->assertEquals(2, $array['user_id']);
             $this->assertEquals('client-id', $array['client_id']);
@@ -31,7 +31,7 @@ class BridgeAccessTokenRepositoryTest extends PHPUnit_Framework_TestCase
         $accessToken->setExpiryDateTime($expiration);
         $accessToken->setClient(new Laravel\Passport\Bridge\Client('client-id', 'name', 'redirect'));
 
-        $repository = new Laravel\Passport\Bridge\AccessTokenRepository($tokenRepository);
+        $repository = new Laravel\Passport\Bridge\AccessTokenRepository($database);
 
         $repository->persistNewAccessToken($accessToken);
     }
