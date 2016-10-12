817e906e84abe348054d5d4f871c5ab117dc3fd4
softdream815 <uyg0815@gmail.com>
2016-10-12 10:28:55 -0500

    formatting

--- src/Http/Middleware/CreateFreshApiToken.php
+++ src/Http/Middleware/CreateFreshApiToken.php
@@ -16,6 +16,13 @@ class CreateFreshApiToken
     protected $cookieFactory;
 
     /**
+     * The authentication guard.
+     *
+     * @var string
+     */
+    protected $guard;
+
+    /**
      * Create a new middleware instance.
      *
      * @param  ApiTokenCookieFactory  $cookieFactory
@@ -36,6 +43,8 @@ class CreateFreshApiToken
      */
     public function handle($request, Closure $next, $guard = null)
     {
+        $this->guard = $guard;
+
         $response = $next($request);
 
         if ($this->shouldReceiveFreshToken($request, $response)) {
@@ -68,7 +77,7 @@ class CreateFreshApiToken
      */
     protected function requestShouldReceiveFreshToken($request)
     {
-        return $request->isMethod('GET') && $request->user();
+        return $request->isMethod('GET') && $request->user($this->guard);
     }
 
     /**
