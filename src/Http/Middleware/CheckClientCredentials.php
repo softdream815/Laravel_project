5ba6dc3d6541a9a11845dc160c434fb263846cb7
softdream815 <uyg0815@gmail.com>
2016-10-24 15:16:51 +0200

    Bump symfony/psr-http-message-bridge version

--- src/Http/Middleware/CheckClientCredentials.php
+++ src/Http/Middleware/CheckClientCredentials.php
@@ -37,21 +37,15 @@ class CheckClientCredentials
      *
      * @throws \Illuminate\Auth\AuthenticationException
      */
-    public function handle($request, Closure $next, ...$scopes)
+    public function handle($request, Closure $next)
     {
         $psr = (new DiactorosFactory)->createRequest($request);
 
         try{
-            $psr = $this->server->validateAuthenticatedRequest($psr);
+            $this->server->validateAuthenticatedRequest($psr);
         } catch (OAuthServerException $e) {
             throw new AuthenticationException;
         }
-
-        foreach ($scopes as $scope) {
-           if (!in_array($scope,$psr->getAttribute('oauth_scopes'))) {
-             throw new AuthenticationException;
-           }
-         }
 
         return $next($request);
     }
